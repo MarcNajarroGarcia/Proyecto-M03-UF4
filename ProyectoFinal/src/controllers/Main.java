0c9763fb9595ed10376f0ddd1631737769736f5e
MarcNajarroGarcia <121028970+MarcNajarroGarcia@users.noreply.github.com>
2023-04-11 12:34:42 +0200

    Add files via upload

---
+++ ProyectoFinal/src/controllers/Main.java
@@ -0,0 +1,15 @@
+package controllers;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+        }
+
+
+
+
+}
---
+++ ProyectoFinal/src/models/Puesto.java
@@ -0,0 +1,5 @@
+package models;
+
+public enum Puesto {
+    Jefe, Empleado;
+}
