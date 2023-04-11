1534887dce5980275b5805396bc5446d92add7ee
MarcNajarroGarcia <121028970+MarcNajarroGarcia@users.noreply.github.com>
2023-04-11 12:30:57 +0200

    Add files via upload

---
+++ Parentesco.java
@@ -0,0 +1,6 @@
+package models;
+
+public enum Parentesco {
+    //ENUM DE PARENTESCO PARA FAMILIARES
+    Padre, Madre, Hijo, Hija, Hermano, Hermana, Abuelo, Abuela, Tio, Tia, Primo, Prima, Sobrino, Sobrina, Cuñado, Cuñada, Yerno, Nuera, Suegro, Suegra;
+}
