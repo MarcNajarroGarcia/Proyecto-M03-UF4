9b8890c55bdbff1d8cb3f8b5eb2b63b99aafffcf
MarcNajarroGarcia <121028970+MarcNajarroGarcia@users.noreply.github.com>
2023-04-11 18:42:54 +0200

    Delete ProyectoFinal directory

--- ProyectoFinal/src/models/Parentesco.java
+++
@@ -1,6 +0,0 @@
-package models;
-
-public enum Parentesco {
-    //ENUM DE PARENTESCO PARA FAMILIARES
-    Padre, Madre, Hijo, Hija, Hermano, Hermana, Abuelo, Abuela, Tio, Tia, Primo, Prima, Sobrino, Sobrina, Cuñado, Cuñada, Yerno, Nuera, Suegro, Suegra;
-}
